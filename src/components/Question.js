3a53c5cf44c2f921dc20476ba90ce9047d02a0ef
Abdulhamid <abdulhamidhersi@gmail.com>
2024-12-04 15:10:45 +0300

    complete

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+    }
+
+
+    return () => clearTimeout(timer);
+  }, [timeRemaining, onAnswered]); 
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
 
+
   const { id, prompt, answers, correctIndex } = question;
+
 
   return (
     <>
@@ -28,5 +46,6 @@ function Question({ question, onAnswered }) {
     </>
   );
 }
+
 
 export default Question;
